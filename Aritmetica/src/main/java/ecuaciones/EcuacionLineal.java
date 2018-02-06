308e3375037df1a2391ab9e10d05242a513e82cd
Mugen <al288175@uji.es>
2018-02-06 12:35:39 +0100

    Add basic skel

---
+++ Aritmetica/src/main/java/ecuaciones/EcuacionLineal.java
@@ -0,0 +1,31 @@
+package ecuaciones;
+
+public class EcuacionLineal {
+
+    public float getX() { return x; }
+
+    public float getA() { return a; }
+
+    public float getB() { return b; }
+
+    public void setX(float x) { this.x = x; }
+
+    public void setA(float a) { this.a = a; }
+
+    public void setB(float b) { this.b = b; }
+
+    private float x, a, b;
+
+    EcuacionLineal(){
+        super();
+    }
+
+    public float solve(){
+        x = (-b)/a;
+    }
+
+
+
+
+
+}
--- Aritmetica/src/test/java/ecuaciones/EcuacionLinealTest.java
+++ Aritmetica/src/test/java/ecuaciones/EcuacionLinealTest.java
@@ -7,19 +7,21 @@ import org.junit.Test;
 //import ecuacion.EcuacionLineal;
 
 public class EcuacionLinealTest {
+	@Test
+	public void terminoIndependienteCeroTest() {
+		EcuacionLineal ecuacionLineal = new EcuacionLineal(1, 0); // x + 0 = 0
+        ecuacionLineal.setA(1f);
+        ecuacionLineal.setB(0f);
 
-//	@Test
-//	public void terminoIndependienteCeroTest() {
-////		EcuacionLineal ecuacionLineal = new EcuacionLineal(1, 0); // x + 0 = 0
-//		// A partir de aquí, el código que comprueba que la solución es correcta
-//	}
-//
-//	@Test
-//	public void distintosDeCeroTest() {
-//		EcuacionLineal ecuacionLineal = new EcuacionLineal(1, 1); // x + 1 = 0
-//		// A partir de aquí, el código que comprueba que la solución es correcta
-//
-//		ecuacionLineal = new EcuacionLineal(2, 1); // 2x + 1 = 0
-//		// A partir de aquí, el código que comprueba que la solución es correcta
-//	}
+		// A partir de aquí, el código que comprueba que la solución es correcta
+	}
+
+	@Test
+	public void distintosDeCeroTest() {
+		EcuacionLineal ecuacionLineal = new EcuacionLineal(1, 1); // x + 1 = 0
+		// A partir de aquí, el código que comprueba que la solución es correcta
+
+		ecuacionLineal = new EcuacionLineal(2, 1); // 2x + 1 = 0
+		// A partir de aquí, el código que comprueba que la solución es correcta
+	}
 }
